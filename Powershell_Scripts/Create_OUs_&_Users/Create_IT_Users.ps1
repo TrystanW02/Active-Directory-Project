6f694264491c13cd564375b42a2d93947d1b8f5a
Trystan Williams <trystanwilliams2002@outlook.com>
2025-04-17 11:48:03 -0500

    Add files via upload

---
+++ Powershell_Scripts/Create_OUs_&_Users/Create_IT_Users.ps1
@@ -0,0 +1,24 @@
+﻿# ----- Edit these Variables for your own Use Case ----- #
+$PASSWORD_FOR_USERS   = "Il0vePCs"
+$USER_FIRST_LAST_LIST = Get-Content .\IT_Names.txt
+# ------------------------------------------------------ #
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+New-ADOrganizationalUnit -Name _IT -ProtectedFromAccidentalDeletion $false
+
+foreach ($n in $USER_FIRST_LAST_LIST) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+
+    New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_IT,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
