fb187e0d23e4427536db3ff1902d6826fe3ac303
Trystan Williams <trystanwilliams2002@outlook.com>
2025-04-17 12:17:50 -0500

    Add files via upload

---
+++ Powershell_Scripts/Create_OUs_&_Users/Create_CSuite_Users.ps1
@@ -0,0 +1,24 @@
+﻿# ----- Edit these Variables for your own Use Case ----- #
+$PASSWORD_FOR_USERS   = "Il0veNumber$"
+$USER_FIRST_LAST_LIST = Get-Content .\CSuite_Names.txt
+# ------------------------------------------------------ #
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+New-ADOrganizationalUnit -Name _CSUITE -ProtectedFromAccidentalDeletion $false
+
+foreach ($n in $USER_FIRST_LAST_LIST) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+    
+    New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_CSUITE,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
